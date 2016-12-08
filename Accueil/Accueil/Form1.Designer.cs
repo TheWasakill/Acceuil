7740a50470cfe05aa4ef1639a016c0d0152b02ca
Aurelien GEOFFRAY <aurelienwasakill@gmail.com>
2016-12-08 16:31:34 +0100

    sol sur bouton

--- Accueil/Accueil/Form1.Designer.cs
+++ Accueil/Accueil/Form1.Designer.cs
@@ -29,34 +29,38 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Musique = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_Musique
             // 
-            this.button1.Location = new System.Drawing.Point(143, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_Musique.BackColor = System.Drawing.Color.Black;
+            this.btn_Musique.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Musique.BackgroundImage")));
+            this.btn_Musique.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_Musique.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Musique.Location = new System.Drawing.Point(141, 12);
+            this.btn_Musique.Name = "btn_Musique";
+            this.btn_Musique.Size = new System.Drawing.Size(86, 34);
+            this.btn_Musique.TabIndex = 0;
+            this.btn_Musique.Text = "Musiques";
+            this.btn_Musique.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(143, 71);
+            this.button2.Location = new System.Drawing.Point(141, 63);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.Size = new System.Drawing.Size(86, 34);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(143, 126);
+            this.button3.Location = new System.Drawing.Point(141, 115);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.Size = new System.Drawing.Size(86, 34);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
@@ -71,7 +75,7 @@
             this.ClientSize = new System.Drawing.Size(380, 317);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Musique);
             this.MaximumSize = new System.Drawing.Size(396, 355);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -82,7 +86,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Musique;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
     }
