a3c0d889bc99dd4a48bd5dba57c56a0156325537
TheWasakill <aurelienwasakill@gmail.com>
2016-12-11 18:20:47 +0100

    btn recherche musique

--- Accueil/Accueil/frm_Musique.Designer.cs
+++ Accueil/Accueil/frm_Musique.Designer.cs
@@ -31,11 +31,18 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Musique));
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.lbx_Musique = new System.Windows.Forms.ListBox();
+            this.btn_ChercherMusique = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Rock",
+            "Pop",
+            "Electro",
+            "Heavy Metal",
+            "Opening"});
             this.comboBox1.Location = new System.Drawing.Point(12, 12);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(151, 21);
@@ -51,13 +58,24 @@
             this.lbx_Musique.Size = new System.Drawing.Size(211, 381);
             this.lbx_Musique.TabIndex = 1;
             // 
+            // btn_ChercherMusique
+            // 
+            this.btn_ChercherMusique.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btn_ChercherMusique.Location = new System.Drawing.Point(12, 39);
+            this.btn_ChercherMusique.Name = "btn_ChercherMusique";
+            this.btn_ChercherMusique.Size = new System.Drawing.Size(88, 43);
+            this.btn_ChercherMusique.TabIndex = 2;
+            this.btn_ChercherMusique.Text = "Recherche";
+            this.btn_ChercherMusique.UseVisualStyleBackColor = true;
+            // 
             // frm_Musique
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(421, 412);
+            this.ClientSize = new System.Drawing.Size(421, 411);
+            this.Controls.Add(this.btn_ChercherMusique);
             this.Controls.Add(this.lbx_Musique);
             this.Controls.Add(this.comboBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -74,5 +92,6 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ListBox lbx_Musique;
+        private System.Windows.Forms.Button btn_ChercherMusique;
     }
 }
